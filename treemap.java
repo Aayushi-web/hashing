2a428b342aa83754683433830224ae5abb478b2a
Aayushi Mishra <mishraaayushi66@gmail.com>
2022-10-09 13:35:41 +0530

    treeMap

---
+++ treemap.java
@@ -0,0 +1,19 @@
+import java.util.TreeMap;
+
+public class treemap {
+   public static void main(String[] args) {
+    TreeMap<String,Integer>tm=  new TreeMap<>();
+    tm.put("aayuhi`", 20);
+    tm.put("yuhi1`", 0);
+    tm.put("aiuhi2`", 20);
+    tm.put("aayuhi3`", 0);
+System.out.println(tm);
+//get function
+tm.get("aayuhi`");
+    tm.get("Kayuhi1`");
+    tm.get("aiuhi2`"
+    );
+    tm.get("aayuhi3`");
+    //opeartion= get,put,remove,isEmpty,size
+   } 
+}
